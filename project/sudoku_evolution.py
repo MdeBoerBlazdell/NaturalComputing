f492d79c61a3d62d7f04f7e052f7990045c57184
Max de Boer-Blazdell <m.deboer-blazdell@ru.nl>
2024-06-02 21:26:19 +0200

    change way of getting results

--- project/sudoku_evolution.py
+++ project/sudoku_evolution.py
@@ -19,80 +19,6 @@ K_SELECT = 5 # TODO experiment with value
 NUM_CHILDREN = 10
 MAX_GENERATIONS = 10000
 
-'''
-def solve_sudoku(sudoku):
-    solved = False
-    gens = 0
-
-    # The original sudoku is the first parent
-    parents = [sudoku.copy()]
-    while not solved and gens < MAX_GENERATIONS:
-        children = generate_children(sudoku, parents)
-        print(f"Generation {gens} has generated {len(children)} children")
-        fitness_scores = []
-
-        for c in children:
-            # Check whether any child is the solved sudoku
-            if valid_sudoku(c):
-                return (True, c, gens)
-            # Compute and store fitness of children
-            fitness_scores.append((c, fitness_from_sudoku(c)))
-
-        total_fitness = np.sum(f for c, f in fitness_scores)
-
-        # Sample parents to seed the next generation
-        new_parents = []
-
-        for c, f in fitness_scores:
-            if random.random() <= (f / (total_fitness - f)):
-                new_parents.append(c)
-
-        # Failsafe: if no parents get sampled, sample parent with highest fitness
-        if len(new_parents) == 0:
-            new_parents.append(max(fitness_scores, key=lambda x: x[1])[0])
-
-        parents = new_parents
-        print(f"Generation {gens} sampled {len(parents)} new parents")
-        gens += 1
-    return (False, sudoku, gens)
-
-def generate_children(sudoku, parents):
-    children = []
-
-    # Store which fields we are free to manipulate without changing
-    # the original sudoku
-    positions = determine_open_fields(sudoku)
-
-    # for p in range(len(parents)):
-    while len(children) < NUM_CHILDREN:
-        p = random.choice(parents)
-        child = p.copy()
-
-        # Make sure the child is a complete sudoku
-        vals = available_values(child)
-        if not len(vals) == 0:
-            for i, j in positions:
-                if child[i][j] == 0:
-                    val = random.choice(vals)
-                    child[i][j] = val
-                    vals.remove(val)
-
-        # if random.random() <= CROSSOVER_RATE:
-            # TODO: Implement crossover between parents
-            # place_holder = 1
-
-        if random.random() <= MUTATION_RATE:
-            # Mutation: swap two digits in the sudoku puzzle
-            i1, j1 = random.choice(positions)
-            i2, j2 = random.choice(positions)
-            temp = child[i1][j1]
-            child[i1][j1] = child[i2][j2]
-            child[i2][j2] = temp
-        children.append(child)
-
-    return children
-'''
-
 def evolve_solution(
     sudoku,
     MAX_GENERATIONS=MAX_GENERATIONS,
@@ -112,16 +38,15 @@ def evolve_solution(
         previous_generation.append(vals)
 
     for generation in range(0, MAX_GENERATIONS):
+        # For testing purposes and to generate result: store highest found fitness over all past generations
         best_in_gen = max([local_fitness_from_values(sudoku, open_fields, c) for c in previous_generation])
-        best_fitness_per_generation.append(best_in_gen)
+        if best_in_gen > best_fitness:
+            best_fitness = best_in_gen
 
-        print(f"{generation} : {best_in_gen}")
-        # cum_fitness = np.sum(
-        #     [local_fitness_from_values(sudoku, open_fields, c) for c in previous_generation]
-        # )
-        # weights = [
-        #     local_fitness_from_values(sudoku, open_fields, c) / cum_fitness for c in previous_generation
-        # ]
+        best_fitness_per_generation.append(best_fitness)
+
+        # print(f"{generation} : {best_in_gen}")
+    
 
         new_children = []
         for _ in range(0, NR_CHILDREN // 2):
--- project/sudoku_solver.py
+++ project/sudoku_solver.py
@@ -2,7 +2,12 @@ from sudoku import parse_sudoku, pretty_print
 from sudoku_evolution import evolve_solution
 import matplotlib.pyplot as plt
 
-sudoku = parse_sudoku("./project/sudoku1_8v.txt")
+from timeit import default_timer as timer
+from datetime import timedelta
+
+start = timer()
+
+sudoku = parse_sudoku("sudokus/sudoku1_8v.txt")
 # constraints = initialise_constraints(sudoku) # You should not need to modify this
 # found, solution, gens = solve_sudoku(sudoku)
 # if(found):
@@ -13,7 +18,11 @@ sudoku = parse_sudoku("./project/sudoku1_8v.txt")
 iterations, solution = evolve_solution(sudoku)
 if iterations == 9_999:
     plt.plot(solution)
-    plt.savefig("./project/results.png")
+    plt.savefig("results.png")
 if iterations < 9_999:    
     print(f"Solved after {iterations} generations")
     pretty_print(solution)
+
+
+end = timer()
+print(timedelta(seconds=end-start))
