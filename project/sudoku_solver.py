e427f48e7594e0f20b18d118f06554a1355f5eea
Luuk Kornegoor <luukkornegoor@gmail.com>
2024-06-10 19:23:35 +0200

    Final code, cleaned up and additional experiment inputs

--- project/sudoku_solver.py
+++ project/sudoku_solver.py
@@ -3,13 +3,6 @@ from sudoku_evolution import evolve_solution
 import matplotlib.pyplot as plt
 
 sudoku = parse_sudoku("./project/sudoku1_8v.txt")
-# constraints = initialise_constraints(sudoku) # You should not need to modify this
-# found, solution, gens = solve_sudoku(sudoku)
-# if(found):
-#     print(f"Solution found after {gens} generations! \n Solution: ")
-#     pretty_print(solution)
-# else:
-#     print(f"No solution found after {gens} generations")
 iterations, solution = evolve_solution(sudoku)
 if iterations == 9_999:
     plt.plot(solution)
