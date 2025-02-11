32dc158e352c50258d355a78097d78488a4fb22b
Luuk Kornegoor <luukkornegoor@gmail.com>
2025-02-11 16:48:42 +0100

    add third sudoku as well as full experimental setup

--- project/sudoku_evolution.py
+++ project/sudoku_evolution.py
@@ -13,19 +13,12 @@ import numpy as np
 import random
 import heapq
 
-# Hyperparameters of the evolutionary algorithm
-# TODO Experiment with full range of K_SELECT as well as MUTATION_RATE (or at least justify choices)
-MUTATION_RATE = 0.01
-K_SELECT = 7
-NUM_CHILDREN = 10
-MAX_GENERATIONS = 10000
-
 def evolve_solution(
     sudoku,
-    MAX_GENERATIONS=MAX_GENERATIONS,
-    MUTATION_RATE=MUTATION_RATE,
-    NR_CHILDREN=NUM_CHILDREN,
-    K_SELECT = K_SELECT
+    max_generations,
+    mutation_rate,
+    nr_children,
+    k_select
 ):
     values = available_values(sudoku)
     open_fields = determine_open_fields(sudoku)
@@ -35,11 +28,11 @@ def evolve_solution(
 
     # Start with an initial set of parents, derived from the original sudoku configuration
     previous_generation = []
-    for _ in range(0, NR_CHILDREN):
+    for _ in range(0, nr_children):
         vals = random.sample(values, len(values))
         previous_generation.append(vals)
 
-    for generation in range(0, MAX_GENERATIONS):
+    for generation in range(0, max_generations):
         # For testing purposes and to generate result: store highest found fitness over all past generations
         best_in_gen = max([local_fitness_from_values(sudoku, open_fields, c) for c in previous_generation])
         best_fitness_per_generation.append(best_in_gen)
@@ -47,29 +40,32 @@ def evolve_solution(
         #print(f"{generation} : {best_in_gen}")
 
         # Fitness-weighed selection
-        #cum_fitness = np.sum(
-        #    [local_fitness_from_values(sudoku, open_fields, c) for c in previous_generation]
-        #    )
-        #weights = [
-        #     local_fitness_from_values(sudoku, open_fields, c) / cum_fitness for c in previous_generation
-        #    ]
+        cum_fitness = 0
+        weights = []
+        
+        cum_fitness = np.sum(
+            [local_fitness_from_values(sudoku, open_fields, c) for c in previous_generation]
+            )
+        weights = [
+            local_fitness_from_values(sudoku, open_fields, c) / cum_fitness for c in previous_generation
+            ]
 
         new_children = []
-        for _ in range(0, NR_CHILDREN // 2):
+        for _ in range(0, nr_children // 2):
             # Fitness-weighed selection
-            # parents = np.random.choice(previous_generation, size = 2, replace = False, p = weights)
+            parents = random.choices(previous_generation, weights = weights, k = 2)
 
             # Tournament selection
-            parent_pool = random.sample(previous_generation, k=K_SELECT)
-            parent_pool_fitness = [(p, local_fitness_from_values(sudoku, open_fields, p)) for p in parent_pool]
-            sorted_pool = sorted(parent_pool_fitness, key = lambda p: p[1])[::-1]
-            parents = [p[0] for p in sorted_pool][:2]
+            #parent_pool = random.sample(previous_generation, k=k_select)
+            #parent_pool_fitness = [(p, local_fitness_from_values(sudoku, open_fields, p)) for p in parent_pool]
+            #sorted_pool = sorted(parent_pool_fitness, key = lambda p: p[1])[::-1]
+            #parents = [p[0] for p in sorted_pool][:2]
 
             child1, child2 = crossover(parents)
             # For both children, we mutate with a probability of MUTATION_RATE
-            if random.random() < MUTATION_RATE:
+            if random.random() < mutation_rate:
                 child1 = mutate(child1)
-            if random.random() < MUTATION_RATE:
+            if random.random() < mutation_rate:
                 child2 = mutate(child2)
             new_children.append(child1)
             new_children.append(child2)
@@ -81,7 +77,7 @@ def evolve_solution(
         previous_generation = new_children
         best_fitness_per_generation.append(best_fitness)
 
-    print(f"No solution found after {MAX_GENERATIONS} generations")
+    # print(f"No solution found after {max_generations} generations")
     return generation, best_fitness_per_generation
 
 def crossover(parents):
--- project/sudoku_solver.py
+++ project/sudoku_solver.py
@@ -15,17 +15,33 @@ def solving_experiment(trials, sudoku, max_gens, m, c, k):
     gens = []
     for i in range(trials):
         iterations, solution = evolve_solution(sudoku=sudoku, 
-                                               MAX_GENERATIONS=max_gens, 
-                                               MUTATION_RATE=m,
-                                               NR_CHILDREN=c,
-                                               K_SELECT=k)
+                                               max_generations=max_gens, 
+                                               mutation_rate=m,
+                                               nr_children=c,
+                                               k_select=k)
         gens.append(iterations)
-    print(gens)
+    return gens
 
-for s in [5,6,7,8]:
-    sudoku_string = f"./project/sudokus/sudoku2_{s}v.txt"
-    sudoku = parse_sudoku(sudoku_string)
-    for k in [2,3,4,5,6,7,8,9,10]:
-        for m in [0.01, 0.05, 0.1]:
-            print(f"Gens of {s}v, k={k}, m={m}:")
-            solving_experiment(12, sudoku, 10000, m, 10, k)
+# Fitness weighed selection (edit sudoku_evolution.evolve_sudoku accordingly)
+for s in range(5,9):
+    for m in [0.01, 0.05, 0.1]:
+        k = 0 
+        sudoku_string = f"./project/sudokus/sudoku3_{s}v.txt"
+        sudoku = parse_sudoku(sudoku_string)
+        gens = solving_experiment(12, sudoku, 10000, m, 10, k)
+        print(f"fw_m{m}_v{s}_p3 = {gens}")
+        print(f"fw_m{m}_v{s}_p3.sort()")
+        print(f"fw_m{m}_v{s}_p3 = fw_m{m}_v{s}_p3[1:-1]")
+        print()
+
+# Tournament selection (edit sudoku_evolution.evolve_sudoku accordingly)
+#for s in range(5,9):
+#    for k in range(2,11):
+#        for m in [0.01, 0.05, 0.1]:
+#            sudoku_string = f"./project/sudokus/sudoku3_{s}v.txt"
+#            sudoku = parse_sudoku(sudoku_string)
+#            gens = solving_experiment(12, sudoku, 10000, m, 10, k)
+#            print(f"k{k}_m{m}_v{s}_p3 = {gens}")
+#            print(f"k{k}_m{m}_v{s}_p3.sort()")
+#            print(f"k{k}_m{m}_v{s}_p3 = k{k}_m{m}_v{s}_p3[1:-1]")
+#            print()
